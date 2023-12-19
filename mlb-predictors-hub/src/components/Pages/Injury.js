b5a3ee8cb3680c44c91d9330727b7d9e5728559a
Marcos Zertuche <105753081+Marcos-Zertuche@users.noreply.github.com>
2023-12-18 18:10:47 -0600

    Starting with models

--- mlb-predictors-hub/src/components/Pages/Injury.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function Injury() {
-  return <h1 className='injury'>Pitcher Injury Risk</h1>;
-}
--- mlb-predictors-hub/src/components/Pages/Playoff.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function Playoff() {
-  return <h1 className='playoff'>Playoff Likelihood</h1>;
-}
